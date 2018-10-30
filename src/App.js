8789d05783620991b8295c41c0f118b176cdc4d8
Paul Steimel <paulsteimel@gmail.com>
2018-10-30 15:07:46 -0500

    Change css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './App.css';
 import QuoteGenerator from './quoteGenerator';
 
 const App = () => (
-  <MuiThemeProvider>
     <QuoteGenerator />
-  </MuiThemeProvider>
 );
 
 export default App;
