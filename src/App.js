3df21ee7b3cb06c50809e8fb2f0bbe754216ac82
Paul Steimel <paulsteimel@gmail.com>
2018-10-30 10:28:32 -0500

    Import code from codepen

--- src/App.js
+++ src/App.js
@@ -2,27 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class QuoteGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myApiData: [],
+      apiURL: "https://talaikis.com/api/quotes/random/"
+    };
+
+    this.buttonClicked = this.buttonClicked.bind(this);
+  }
+
+  buttonClicked() {
+    fetch(this.state.apiURL)
+      .then(response => response.json())
+      .then(data => this.setState({ myApiData: data }));
+  }
+
+  componentDidMount() {
+    fetch(this.state.apiURL)
+      .then(response => response.json())
+      .then(data => this.setState({ myApiData: data }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <blockquote id='text'>{this.state.myApiData.quote}
+        </blockquote>
+        <div id='author-area'>
+          <cite id='author'>- {this.state.myApiData.author}
+          </cite>
+        </div>
+        <div></div>
+        <div id='button-area'>
+        <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=` + `"` + encodeURIComponent(this.state.myApiData.quote) + `" -` + this.state.myApiData.author}target='_blank'>Tweet Quote</a>
+        <button id='new-quote' onClick={this.buttonClicked.bind(this)}>New Quote</button>
+        </div>
+      </section>
     );
   }
 }
 
-export default App;
+export default QuoteGenerator;
